677fdb4982b082f4b44dfdfbebc1d4eef008f8de
huey-J <wkdgus7113@gmail.com>
2022-11-30 17:04:29 +0900

    initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+  @Value("${data.name}")
+  private String dataName;
+
+  @GetMapping("")
+  public String helloRealUser() {
+    return "hello : " + dataName;
+  }
+
+}
