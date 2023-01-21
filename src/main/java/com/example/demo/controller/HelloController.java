13d7416a9bca78261a7e8b4b66f710bd52bf2f45
huey-J <wkdgus7113@gmail.com>
2023-01-21 16:07:12 +0900

    add redis with docker

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,15 @@ public class HelloController {
   @Value("${data.name}")
   private String dataName;
 
+  @Autowired
+  private RedisTemplate<String, Object> redisTemplate;
+
   @GetMapping("")
   public String helloRealUser() {
+    redisTemplate.opsForValue().set(
+        "RT:" + 1L,
+        "hihihi"
+    );
     return "hello : " + dataName;
   }
 
